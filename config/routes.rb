cb62e460780711edd4687d7ba82a6258a5a70f80
ciupkajakub <ciupkajakub@gmail.com>
2021-10-18 09:59:20 +0200

    Added devise and set the redirection to home page after registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
